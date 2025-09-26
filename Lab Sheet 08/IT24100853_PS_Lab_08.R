12402f11533159abc4561b1833062d0439d1d505
IT24100853 <IT24100853@MY.SLIIT.LK>
2025-09-26 16:24:39 +0530

    IT24100853 - Lab 08

---
+++ Lab Sheet 08/IT24100853_PS_Lab_08.R
@@ -0,0 +1,38 @@
+setwd("C:\\Users\\IT24100853\\Desktop\\IT24100853 PS 08")
+
+data <- read.table("Exercise - LaptopsWeights.txt", header=TRUE)
+fix(data)
+attach(data)
+
+## Question 01
+popmn <- mean(Weight.kg.)
+popvar <- var(Weight.kg.) * (length(Weight.kg.)-1)/length(Weight.kg.)
+popSD <- sqrt(popvar)
+
+popmn
+popvar
+popSD
+
+## Question 02
+samples <- c()
+n <- c()
+for (i in 1:25){
+  s <- sample(Weight.kg., 6, replace=TRUE)
+  samples <- cbind(samples, s)
+  n <- c(n,paste('S',i))
+}
+colnames(samples) <- n
+
+s.means <- apply(samples, 2, mean)
+s.var   <- apply(samples, 2, var)
+s.SD    <- sqrt(s.var)
+
+s.means
+s.SD
+
+## Question 03
+mean_smeans <- mean(s.means)
+sd_smeans   <- sd(s.means)
+
+mean_smeans
+sd_smeans
